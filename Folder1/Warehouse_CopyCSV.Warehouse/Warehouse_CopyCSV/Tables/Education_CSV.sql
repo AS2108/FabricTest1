70f18467a4f4c3cd4c55777f28192eda941675f0
AS2108 <120604083+AS2108@users.noreply.github.com>
2025-02-27 12:50:59 +1300

    Committing 6 items from workspace 84578e44-5c44-4733-b3f2-5a73b650bf96

---
+++ Folder1/Warehouse_CopyCSV.Warehouse/Warehouse_CopyCSV/Tables/Education_CSV.sql
@@ -0,0 +1,24 @@
+CREATE TABLE [Warehouse_CopyCSV].[Education_CSV] (
+
+	[Student_ID] varchar(8000) NULL, 
+	[Age] varchar(8000) NULL, 
+	[Gender] varchar(8000) NULL, 
+	[High_School_GPA] varchar(8000) NULL, 
+	[SAT_Score] varchar(8000) NULL, 
+	[University_Ranking] varchar(8000) NULL, 
+	[University_GPA] varchar(8000) NULL, 
+	[Field_of_Study] varchar(8000) NULL, 
+	[Internships_Completed] varchar(8000) NULL, 
+	[Projects_Completed] varchar(8000) NULL, 
+	[Certifications] varchar(8000) NULL, 
+	[Soft_Skills_Score] varchar(8000) NULL, 
+	[Networking_Score] varchar(8000) NULL, 
+	[Job_Offers] varchar(8000) NULL, 
+	[Starting_Salary] varchar(8000) NULL, 
+	[Career_Satisfaction] varchar(8000) NULL, 
+	[Years_to_Promotion] varchar(8000) NULL, 
+	[Current_Job_Level] varchar(8000) NULL, 
+	[Work_Life_Balance] varchar(8000) NULL, 
+	[Entrepreneurship] varchar(8000) NULL
+);
+
